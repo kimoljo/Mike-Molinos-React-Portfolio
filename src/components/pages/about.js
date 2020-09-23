6ceeee7fa6360a1996e7caae5663e1723e21dcf6
kimoljo <msm.com@gmail.com>
2020-09-23 16:28:45 -0500

    message

--- src/components/pages/about.js
+++ src/components/pages/about.js
@@ -13,23 +13,8 @@ export default function () {
                 }}
             />
             <div className="right-column">
-                Maecenas faucibus mollis interdum. Integer posuere erat a ante venenatis
-                dapibus posuere velit aliquet. Sed posuere consectetur est at lobortis.
-                Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui.
-                Aenean lacinia bibendum nulla sed consectetur. Maecenas sed diam eget
-                risus varius blandit sit amet non magna. Morbi leo risus, porta ac
-                consectetur ac, vestibulum at eros. Donec id elit non mi porta gravida
-                at eget metus. Donec sed odio dui. Cras mattis consectetur purus sit
-                amet fermentum. Etiam porta sem malesuada magna mollis euismod. Nulla
-                vitae elit libero, a pharetra augue. Aenean eu leo quam. Pellentesque
-                ornare sem lacinia quam venenatis vestibulum. Duis mollis, est non
-                commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec
-                elit. Praesent commodo cursus magna, vel scelerisque nisl consectetur
-                et. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent
-                commodo cursus magna, vel scelerisque nisl consectetur et. Nullam quis
-                risus eget urna mollis ornare vel eu leo. Morbi leo risus, porta ac
-                consectetur ac, vestibulum at eros. Vestibulum id ligula porta felis
-                euismod semper.
+                <p>Mike Molinos is a tech professional and an avid full-stack web application developer living in Utah County’s silicone slopes. His interest in the tech industry started in 2002 when he completed his AS degree in information technology He complemented that tech passion with entrepreneurial tendencies upon earning his Business degree from Brigham Young University.</p>
+                <p>Mike is proficient in HTML5, CSS3, JavaScript, JSX, Axios, SCSS/SASS, Flexbox, CSS Grid, ReactJS, Git, MySQL, ASP.Net, MVC, Crud, Visual Studio Code, Postman, Heroku, GitHub, and Adobe Photoshop & Illustrator. He has experience integrating third-party APIs and DOM manipulations in responsive development projects.</p>
             </div>
         </div>
     );
